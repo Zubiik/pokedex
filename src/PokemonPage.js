4b96c7f351113833df0fd44622a242e818454818
Zubiik <elodiefrappat08@gmail.com>
2021-05-25 08:49:52 -0400

    first tree structure

---
+++ src/PokemonPage.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function PokemonPage() {
+  return (
+    <div>
+     coucou
+    </div>
+  );
+}
+
---
+++ src/searchPage/SearchPage.js
@@ -0,0 +1,10 @@
+
+
+export default function PokemonPage() {
+  return (
+    <div>
+     coucou
+    </div>
+  );
+}
+
