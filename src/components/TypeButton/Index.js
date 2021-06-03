ab60e5c2bd3cf45279c8f76b5992a77faae0e47a
Zubiik <elodiefrappat08@gmail.com>
2021-06-03 12:35:17 -0400

    feat : create type button

---
+++ src/components/TypeButton/Index.js
@@ -0,0 +1,11 @@
+import GetPokemon from "../../api/GetPokemon";
+import {} from "./styled.js";
+export default function Button({
+  pokemon,
+  pokemonInfo,
+  setPokemonInfo,
+  pokemonDescription,
+  setPokemonDescription,
+}) {
+  return <div>{pokemonInfo}</div>;
+}
