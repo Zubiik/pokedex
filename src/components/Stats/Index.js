13a7f8c71b86825102ae9e383963cc3db3ae84bb
Zubiik <elodiefrappat08@gmail.com>
2021-06-09 11:14:34 -0400

    feat: stat loop and map

--- src/components/Stats/Index.js
+++ src/components/Stats/Index.js
@@ -1,3 +1,15 @@
-export default function Stats() {
-  return <div></div>;
+export default function Stats({ pokemonInfo }) {
+  return (
+    <div>
+      {pokemonInfo.stats.map((stat, index) => {
+        const division = stat.base_stat / 10;
+        const statResult = Math.round(division);
+        var toto = [];
+        for (let i = 0; i < statResult; i++) {
+          toto.push(<div>{i}</div>);
+        }
+        return toto.map((item) => item);
+      })}
+    </div>
+  );
 }
