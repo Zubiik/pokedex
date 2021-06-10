e042991b2dfd8bdb9d13a73fdac5dd0c60129f1f
Zubiik <elodiefrappat08@gmail.com>
2021-06-10 12:10:01 -0400

    feat: base for header

---
+++ src/components/Header/Index.js
@@ -0,0 +1,14 @@
+import {
+  InfoContainerCustom,
+  TextContainer,
+  TextInfoBox,
+  DescriptionText,
+  TextLabel,
+  AbilitiesContainer,
+  InfoRequestCustom,
+  HandlerContainer,
+} from "./styled";
+
+export default function Header({ pokemon }) {
+  return <div></div>;
+}
