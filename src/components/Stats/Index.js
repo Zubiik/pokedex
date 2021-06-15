d3ded249eeb56bef51334f79f37531028b5e0f2e
Zubiik <elodiefrappat08@gmail.com>
2021-06-15 09:05:28 -0400

    feat: styled stat array

--- src/components/Stats/Index.js
+++ src/components/Stats/Index.js
@@ -1,25 +1,47 @@
-import { StatsContainer, LonelyStat, Item, TextToHide } from "./styled";
+import {
+  StatsContainer,
+  EmptyStat,
+  LonelyStat,
+  Item,
+  TextToHide,
+  TextContainer,
+  ArrayContainer,
+  TextCustom,
+} from "./styled";
 
 export default function Stats({ pokemonInfo }) {
   return (
-    <StatsContainer>
-      {pokemonInfo.stats.map((stat, index) => {
-        const division = stat.base_stat / 10;
-        const statResult = Math.round(division);
-        var toto = [];
-        var fifteen = 15;
-        for (let i = 0; i < statResult; i++) {
-          toto.push(<LonelyStat>{i}</LonelyStat>);
-          for (let i = 0; i < fifteen; i++);
-        }
-        return (
-          <Item>
-            {toto.map((item) => (
-              <TextToHide>{item}</TextToHide>
-            ))}
-          </Item>
-        );
-      })}
-    </StatsContainer>
+    <ArrayContainer>
+      <TextContainer>
+        <TextCustom>HP</TextCustom>
+        <TextCustom>Attack</TextCustom>
+        <TextCustom>Defense</TextCustom>
+        <TextCustom>Spe A</TextCustom>
+        <TextCustom>Spe D</TextCustom>
+        <TextCustom>Speed</TextCustom>
+      </TextContainer>
+      <StatsContainer>
+        {pokemonInfo.stats.map((stat, index) => {
+          const division = stat.base_stat / 10;
+          const statResult = Math.round(division);
+          var toto = [];
+          for (let i = 0; i < 15; i++) {
+            if (i < statResult) {
+              toto.push(<LonelyStat />);
+            } else {
+              toto.push(<EmptyStat />);
+            }
+          }
+
+          return (
+            <Item>
+              {toto.map((item) => (
+                <TextToHide>{item}</TextToHide>
+              ))}
+            </Item>
+          );
+        })}
+      </StatsContainer>
+    </ArrayContainer>
   );
 }
--- src/components/Stats/styled.js
+++ src/components/Stats/styled.js
@@ -1,12 +1,27 @@
 import styled from "@emotion/styled";
 
+export const ArrayContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  background-color: #c4c4c4;
+  max-width: max-content;
+  padding: 0.5rem;
+`;
 export const StatsContainer = styled.div`
   display: flex;
-  border: blue solid 1px;
+  border: #c4c4c4 solid 1px;
+`;
+export const TextContainer = styled.div`
+  display: flex;
 `;
 
+export const TextCustom = styled.div`
+  display: flex;
+  width: 4rem;
+  height: 1rem;
+`;
 export const LonelyStat = styled.div`
-  background-color: blueviolet;
+  background-color: #38a8d5;
   border: white 1px solid;
   width: 4rem;
   height: 1rem;
@@ -15,9 +30,15 @@ export const LonelyStat = styled.div`
 export const Item = styled.div`
   display: flex;
   flex-direction: column-reverse;
-  border: powderblue solid 1px;
 `;
 
 export const TextToHide = styled.p`
   color: blueviolet;
+`;
+
+export const EmptyStat = styled(LonelyStat)`
+  background-color: white;
+  border: #c4c4c4 1px solid;
+  width: 4rem;
+  height: 1rem;
 `;
