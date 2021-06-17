8a1722abcf9a44235329b5a431fa3e6ac703c302
Zubiik <elodiefrappat08@gmail.com>
2021-06-17 07:29:05 -0400

    feat: add 0 before id

--- src/components/header/Index.js
+++ src/components/header/Index.js
@@ -1,9 +1,22 @@
 import { HeaderCustom, TextCustom } from "./styled";
 export default function HeaderTitle({ pokemonInfo }) {
+  const pokemonId = pokemonInfo.id;
+
+  const pokemonNumber = () => {
+    if (pokemonId < 9) {
+      return "00" + pokemonId;
+    }
+    if (pokemonId > 9 && pokemonId < 100) {
+      return "0" + pokemonId;
+    } else {
+      return pokemonId;
+    }
+  };
+
   return (
     <HeaderCustom>
       <TextCustom>{pokemonInfo.name}</TextCustom>
-      <TextCustom>#{pokemonInfo.id}</TextCustom>
+      <TextCustom>#{pokemonNumber()}</TextCustom>
     </HeaderCustom>
   );
 }
