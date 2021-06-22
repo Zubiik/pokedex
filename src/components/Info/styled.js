14561aba923901f31e27deeaa26aa8b2be60eea5
Zubiik <elodiefrappat08@gmail.com>
2021-06-22 09:10:07 -0400

    fix: change tag

--- src/components/Info/styled.js
+++ src/components/Info/styled.js
@@ -23,12 +23,12 @@ export const TextContainer = styled.div`
   flex-direction: column;
 `;
 
-export const TextInfoBox = styled.div`
+export const TextInfoBox = styled.p`
   display: flex;
   justify-content: space-between;
   text-align: right;
 `;
-export const DescriptionText = styled.div`
+export const DescriptionText = styled.p`
   align-items: center;
   flex-direction: column;
   text-align: center;
@@ -51,6 +51,6 @@ export const AbilitiesContainer = styled.div`
   color: ${({ theme }) => theme.colors.orange};
 `;
 
-export const AbilityName = styled.div`
+export const AbilityName = styled.p`
   padding-right: 0.5rem; ;
 `;
