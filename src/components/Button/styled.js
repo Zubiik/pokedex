b5d9d59b20cc19ef4a6ed872f206be5ff0a4ad9d
Zubiik <elodiefrappat08@gmail.com>
2021-06-23 10:12:34 -0400

    fix: px to rem

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
 
 export const SearchButtonCustom = styled.div`
-  margin: 5px;
+  margin: 0.5rem;
   width: 10rem;
   height: 5rem;
   background-color: ${({ theme }) => theme.colors.orange};
-  border-radius: 80px;
+  border-radius: 5rem;
   color: ${({ theme }) => theme.colors.white};
   display: flex;
   justify-content: center;
